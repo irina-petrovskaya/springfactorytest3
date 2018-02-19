493a4f5097bd240b0c683cfd24e08ed91337da8d
irina <irina@me.com>
2018-02-20 01:08:27 +0300

    formatting test corrected (with one condition false)

--- use-myautoconfigurationbundle3/src/test/java/com/mytests/springBoot/autoconfiguration/bundle3_usage/ConditionalOnPropertyTest.java
+++ use-myautoconfigurationbundle3/src/test/java/com/mytests/springBoot/autoconfiguration/bundle3_usage/ConditionalOnPropertyTest.java
@@ -33,40 +33,28 @@ public class ConditionalOnPropertyTest {
     private Environment environment;
 
 
-
-
     @Autowired(required = false)
     private ConfigComponent10 configComponent10;
-    @Autowired(required=false)
+    @Autowired(required = false)
     private Bean10 bean10_from_configcomponent10;
 
 
     @Autowired(required = false)
     private ImportedConfig11 importedConfig11;
-    @Autowired(required=false)
+    @Autowired(required = false)
     private Bean11 bean11_from_importedconfig11;
 
 
     @Autowired(required = false)
     private ImportedConfig12 importedConfig12;
-    @Autowired(required=false)
+    @Autowired(required = false)
     private Bean12 bean12_from_importedconfig12;
 
 
     @Autowired(required = false)
     private ImportedConfig12.ImportedCfg12Inner importedCfg12Inner;
-    @Autowired(required=false)
+    @Autowired(required = false)
     private Bean15 bean15_from_importedcfg12inner;
-
-
-
-
-
-
-
-
-
-
 
 
     @Autowired(required = false)
@@ -75,12 +63,11 @@ public class ConditionalOnPropertyTest {
     private Bean20 bean20_from_configcomp111;
 
 
-
 // mainConfig1:
 //
     @Autowired(required = false)
     private MainConfig1 mainConfig1;
-    @Autowired(required=false)
+    @Autowired(required = false)
     private Bean1 bean1_from_mainconfig1;
 
     // no conditions here except mainconfig1 condition
@@ -90,26 +77,27 @@ public class ConditionalOnPropertyTest {
     private Bean18 bean18_from_importedconfig13;
 
     // no conditions here except mainconfig1 condition
-    @Autowired(required=false)
+    @Autowired(required = false)
     private Bean17 bean17_from_xml;
 
 
     @Test
     public void testMainConfig1() {
-        if(environment.getProperty("myprops.main1").equals("true")) {
+        if (environment.getProperty("myprops.main1").equals("true")) {
             System.out.println("condition for mainconfig1 is ok:");
             System.out.println("bean1_from_mainconfig1 and mainconfig1 beans should be available");
             System.out.println("bean17_from_xml, bean18_from_importedconfig13 and importedConfig13 also");
-        Assert.assertNotNull("mainconfig1 bean",mainConfig1);
-        Assert.assertNotNull("bean1_from_mainconfig1", bean1_from_mainconfig1);
-        Assert.assertNotNull("importedConfig13", importedConfig13);
-        Assert.assertNotNull("bean18_from_importedconfig13", bean18_from_importedconfig13);
-        Assert.assertNotNull("bean17_from_xml", bean17_from_xml);
-        ;} else {
+            Assert.assertNotNull("mainconfig1 bean", mainConfig1);
+            Assert.assertNotNull("bean1_from_mainconfig1", bean1_from_mainconfig1);
+            Assert.assertNotNull("importedConfig13", importedConfig13);
+            Assert.assertNotNull("bean18_from_importedconfig13", bean18_from_importedconfig13);
+            Assert.assertNotNull("bean17_from_xml", bean17_from_xml);
+            ;
+        } else {
             System.out.println("condition for mainconfig1 is broken:");
             System.out.println("bean1_from_mainconfig1 and mainconfig1 beans should be not available");
             System.out.println("bean17_from_xml, bean18_from_importedconfig13 and importedConfig13 also");
-            Assert.assertNull("mainconfig1 bean",mainConfig1);
+            Assert.assertNull("mainconfig1 bean", mainConfig1);
             Assert.assertNull("bean1_from_mainconfig1", bean1_from_mainconfig1);
             Assert.assertNull("importedConfig13", importedConfig13);
             Assert.assertNull("bean18_from_importedconfig13", bean18_from_importedconfig13);
@@ -117,49 +105,59 @@ public class ConditionalOnPropertyTest {
             ;
         }
     }
+
 // ConfigComponent11
 //
     @Autowired(required = false)
     private ConfigComponent11 configComponent11;
     @Autowired(required = false)
     private ConfigComponent11.ConfComp11Inner confComp11Inner;
-    @Autowired(required=false)
+    @Autowired(required = false)
     private Bean16 bean16_from_confcomp11inner;
 
     @Test
     public void testConfigComponent11() {
-        if((environment.getProperty("myprops.configcomp11").equals("true"))&&(environment.getProperty("myprops.configcomp11inner").equals("true"))) {
+        System.out.println("\"myprops.configcomp11\"=" + environment.getProperty("myprops.configcomp11"));
+        System.out.println("\"myprops.configcomp11inner\"=" + environment.getProperty("myprops.configcomp11inner"));
+        if ((environment.getProperty("myprops.configcomp11").equals("true")) && (environment.getProperty("myprops.configcomp11inner").equals("true"))) {
             System.out.println("conditions for confcomp11inner and configcomponent11 are ok:");
             System.out.println("bean16_from_confcomp11inner, configComponent11 and confComp11Inner beans should be available");
 
             Assert.assertNotNull(confComp11Inner);
             Assert.assertNotNull(configComponent11);
-            Assert.assertNotNull(bean16_from_confcomp11inner);}
-        else {
+            Assert.assertNotNull(bean16_from_confcomp11inner);
+        } else {
+            if (!(environment.getProperty("myprops.configcomp11").equals("true"))) {
+                System.out.println("condition for configcomponent11 are broken:");
+                System.out.println("bean16_from_confcomp11inner, configComponent11 and confComp11Inner beans should not be available");
 
-            System.out.println("conditions for confcomp11inner and configcomponent11 are broken:");
-            System.out.println("bean16_from_confcomp11inner, configComponent11 and confComp11Inner beans should not be available");
-
-            Assert.assertNull(configComponent11);
-            Assert.assertNull(confComp11Inner);
-            Assert.assertNull(bean16_from_confcomp11inner);
+                Assert.assertNull(configComponent11);
+                Assert.assertNull(confComp11Inner);
+                Assert.assertNull(bean16_from_confcomp11inner);
+            } else {
+                System.out.println("condition for configcomponent11inner only is broken:");
+                System.out.println("configComponent1 should be available, but bean16_from_confcomp11inner and confComp11Inner beans should not be available");
+                Assert.assertNotNull(configComponent11);
+                Assert.assertNull(confComp11Inner);
+                Assert.assertNull(bean16_from_confcomp11inner);
+            }
         }
     }
+
 // Component10:
 //
     @Autowired(required = false)
     private Component10 component10;
-    @Autowired(required=false)
+    @Autowired(required = false)
     private Bean13 bean13_from_component10;
 
     @Test
     public void testComponent10() {
-        if(environment.getProperty("myprops.component10").equals("true"))
-        {
-            System.out.println("\"myprops.component10\"="+environment.getProperty("myprops.component10"));
+        if (environment.getProperty("myprops.component10").equals("true")) {
+            System.out.println("\"myprops.component10\"=" + environment.getProperty("myprops.component10"));
             System.out.println("bean13_from_component10 and component10 are available");
-            Assert.assertNotNull("component10",component10);
-            Assert.assertNotNull("bean13_from_component10",bean13_from_component10);
+            Assert.assertNotNull("component10", component10);
+            Assert.assertNotNull("bean13_from_component10", bean13_from_component10);
         }
     }
 
@@ -167,22 +165,21 @@ public class ConditionalOnPropertyTest {
 //
     @Autowired(required = false)
     private Component11.Component11Inner component11Inner;
-    @Autowired(required=false)
+    @Autowired(required = false)
     private Bean14 bean14_from_component11inner;
     @Autowired(required = false)
     private Component11 component11;
 
     @Test
     public void testComponent11() {
-        if(environment.getProperty("myprops.component11").equals("true")&&
-                environment.getProperty("myprops.component11inner").equals("true"))
-        {
-            System.out.println("\"myprops.component11\"="+environment.getProperty("myprops.component11"));
-            System.out.println("\"myprops.component11inner\"="+environment.getProperty("myprops.component11inner"));
+        if (environment.getProperty("myprops.component11").equals("true") &&
+                environment.getProperty("myprops.component11inner").equals("true")) {
+            System.out.println("\"myprops.component11\"=" + environment.getProperty("myprops.component11"));
+            System.out.println("\"myprops.component11inner\"=" + environment.getProperty("myprops.component11inner"));
             System.out.println("bean14_from_component11inner, component11inner and component11 are available");
-            Assert.assertNotNull("component11",component11);
-            Assert.assertNotNull("component11inner",component11Inner);
-            Assert.assertNotNull("bean14_from_component11inner",bean14_from_component11inner);
+            Assert.assertNotNull("component11", component11);
+            Assert.assertNotNull("component11inner", component11Inner);
+            Assert.assertNotNull("bean14_from_component11inner", bean14_from_component11inner);
         }
     }
 }
